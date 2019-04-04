4c635966418630f859ebd025f52804a03942c50a
jfeltman <joshuafeltman97@gmail.com>
2019-04-03 22:54:17 -0700

    Base app created

---
+++ iOS Tetris/iOS TetrisTests/iOS_TetrisTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_TetrisTests.swift
+//  iOS TetrisTests
+//
+//  Created by Josh Feltman on 4/3/19.
+//  Copyright © 2019 Josh Feltman. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Tetris
+
+class iOS_TetrisTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
